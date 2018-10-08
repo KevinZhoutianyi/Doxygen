1650c747bb7cf1e45237dcd321b2138b306e9520
OnlyDrinkWater <kevinzhou2@live.cn>
2018-10-08 12:01:52 +0100

    init

---
+++ class.h
@@ -0,0 +1,45 @@
+//***************************************************************************************
+//
+//! \file class.h
+//! This is a test to see how the doxygen generate the html.
+//!
+//! \author    Kevin
+//! \version   V1.0
+//! \date      2018-10-8
+//
+//***************************************************************************************
+
+
+
+#ifndef CLASS_H
+#define CLASS_H
+
+#include "iostream"
+
+
+#define CNT_MAX  10  //!< The maxium number of print
+
+using namespace std;
+
+class My_Fish
+{
+
+    private:
+        int mother;
+        int father;
+
+    public:
+        My_Fish(int temp1,int temp2)
+        {
+            cout<<"MYFISH object is created"<<endl;
+            cout<<temp1<<endl;
+            cout<<temp2<<endl;
+            this->mother=temp1;
+            this->father=temp2;
+        }
+
+
+};
+
+
+#endif // CLASS_H
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "class.h"
+
+int main(void)
+{
+    My_Fish fish(1,2);
+
+    return 0;
+}
